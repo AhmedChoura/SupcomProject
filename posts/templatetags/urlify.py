bc5ff9e88ceabd019d5b2c7f157a3b28213d16a6
CHOURA Ahmed <ahmed.choura@supcom.tn>
2017-11-23 00:37:02 +0100

    fuck5

--- posts/templatetags/urlify.py
+++
@@ -1,8 +0,0 @@
-from urllib import quote_plus
-from django import template
-#get the template library
-register = template.Library()
-# register urlify function as a filter in this library to use directly in templates
-@register.filter
-def urlify(value):
-    return quote_plus(value)
